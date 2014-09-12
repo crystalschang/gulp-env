63dba522d16e89043633bb5be5011ed32deae05f
Russell Matney <russell.matney@gmail.com>
2014-09-11 21:40:55 -0400

    adds test command and test, uses process.cwd()

--- test/test-gulp-env.js
+++ test/test-gulp-env.js
@@ -5,4 +5,12 @@ describe('gulp-env', function() {
   it('should exist', function() {
     expect(env).to.exist;
   });
+
+  it('should add process.env vars from a local file', function() {
+    env({file: "test/mock-env"})
+
+    expect(process.env.STARK).to.equal("direwolf");
+    expect(process.env.BARATHEON).to.equal("stag");
+    expect(process.env.LANNISTER).to.equal("lion");
+  });
 })
