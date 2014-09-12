61ab74f181183a96dcb93210ba91992a65437e08
Russell Matney <russell.matney@gmail.com>
2014-09-11 20:21:03 -0400

    updates to route from source directory

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = function(options) {
   if (options.file) {
-    var env = require("./" + options.file);
+    var env = require(__dirname + "/" + options.file);
 
     for (var prop in env) {
       process.env[prop] = env[prop]
